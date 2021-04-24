a7ebb06e412e7baec04061af7363689b12e9dcbd
Anna Ikoki <ikokianna@gmail.com>
2021-04-24 02:36:44 +0200

    add logo-emoji

--- baby-recipe/src/index.js
+++ baby-recipe/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render( <
     React.StrictMode >
     <
-    App title = "The Recipe App" / >
+    App title = " 🍲 The Recipe App" / >
     <
     /React.StrictMode>,
     document.getElementById('root')
